74130aaf76e34378ec5766a8a021adccfe8a3b14
Ronald van den Broek <ronald.vandenbroek@live.nl>
2020-03-11 17:19:27 +0100

    Formatting code

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,54 +1,54 @@
-const { setHeadlessWhen } = require('@codeceptjs/configure');
+const { setHeadlessWhen } = require('@codeceptjs/configure')
 
 // turn on headless mode when running with HEADLESS=true environment variable
 // HEADLESS=true npx codecept run
-setHeadlessWhen(process.env.HEADLESS);
+setHeadlessWhen(process.env.HEADLESS)
 
 exports.config = {
-  tests: './test/*_test.js',
-  output: './output',
-  include: {
-    I: './step/steps_file.js'
-  },
-  bootstrap: null,
-  mocha: {},
-  name: 'poc-codeceptjs-testcafe-allure',
+    tests: './test/*_test.js',
+    output: './output',
+    include: {
+        I: './step/steps_file.js',
+    },
+    bootstrap: null,
+    mocha: {},
+    name: 'poc-codeceptjs-testcafe-allure',
 
-  helpers: {
-    TestCafe: {
-      url: 'http://localhost',
-      browser: 'firefox',
-      show: false
-    }
-  },
-  multiple: {
-    basic: {
-      chunks: 2,
-      browsers: ['firefox', 'chrome'],
+    helpers: {
+        TestCafe: {
+            url: 'http://localhost',
+            browser: 'firefox',
+            show: false,
+        },
     },
-    parallel: {
-      chunks: 2,
-      browsers: [{
-          browser: 'chrome',
-          windowSize: '1920x1080',
-      }, {
-          browser: 'firefox',
-          windowSize: '1920x1080',
-      }],
-    },
+    multiple: {
+        basic: {
+            chunks: 2,
+            browsers: ['firefox', 'chrome'],
+        },
+        parallel: {
+            chunks: 2,
+            browsers: [{
+                browser: 'chrome',
+                windowSize: '1920x1080',
+            }, {
+                browser: 'firefox',
+                windowSize: '1920x1080',
+            }],
+        },
     // all: {
     //   browsers: ['chrome', 'firefox', 'ie', 'edge', 'safari', 'opera']
     // },
-  },
-  plugins: {
-    retryFailedStep: {
-      enabled: true
     },
-    screenshotOnFail: {
-      enabled: true
+    plugins: {
+        retryFailedStep: {
+            enabled: true,
+        },
+        screenshotOnFail: {
+            enabled: true,
+        },
+        allure: {
+            enabled: true,
+        },
     },
-    allure: {
-      enabled: true
-    }
-  }
 }
